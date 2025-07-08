2c1d5ba6b6ca03a596bfd8930b38a0c6abd1653d
Rosa Vela <rosymer96@gmail.com>
2025-07-08 12:38:37 +0200

    Enlaces anadidos a incones de body

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private router = inject(Router);
+
+
+}
