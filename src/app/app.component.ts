699e2494af5b133faf61e2567ed5fbf0435db7f7
Rosa Vela <rosymer96@gmail.com>
2025-07-08 12:25:30 +0200

    Side-nav completo, sin enlaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import {
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    MatSidenavContainer,
+    MatSidenavContent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'PORTFOLIO-ROSA-VELA';
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,CommonModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  isDrawerOpen = false;
+
+  @ViewChild('drawer') drawer!: MatSidenav;
+
+  toggleSidenav() {
+    this.drawer.toggle();
+  }
+
+  onOpenedChange(opened: boolean) {
+    this.isDrawerOpen = opened;
+  }
+}
