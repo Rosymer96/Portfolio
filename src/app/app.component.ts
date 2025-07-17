418417ac2cc083ef1daf2b42151f90af12fff7bf
Rosa Vela <rosymer96@gmail.com>
2025-07-17 13:16:12 +0200

    Menu-mobile con scroll implemtado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { MatSidenavContainer, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
+import {
+  MatSidenavContainer,
+  MatSidenavContent,
+  MatSidenavModule,
+} from '@angular/material/sidenav';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectPillComponent } from './components/project-pill/project-pill.component';
 import { ProjectsComponent } from './components/projects/projects.component';
@@ -20,8 +24,8 @@ import { ContactComponent } from './components/contact/contact.component';
     ProjectsComponent,
     SkillsComponent,
     ContactComponent,
-    MatSidenavModule
-],
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -29,7 +33,25 @@ export class AppComponent {
   title = 'PORTFOLIO-ROSA-VELA';
   isDrawerOpen = false;
 
+  // Referencias a secciones
+  @ViewChild('homeRef') homeRef!: ElementRef;
+  @ViewChild('aboutRef') aboutRef!: ElementRef;
+  @ViewChild('skillsRef') skillsRef!: ElementRef;
+  @ViewChild('projectsRef') projectsRef!: ElementRef;
+  @ViewChild('contactRef') contactRef!: ElementRef;
+
   onOpenedChange(opened: boolean) {
     this.isDrawerOpen = opened;
+  }
+
+  /**
+   * Scrolls to the specified element on the page.
+   * @param elementId The ID of the element to scroll to.
+   */
+  scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   }
 }
